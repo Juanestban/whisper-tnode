bb8decbce40b15642340c69d9bfda6c7a5d1c0a0
Juan Esteban <juanestban000@gmail.com>
2023-10-27 17:46:42 -0500

    feat: created prerelease file node

---
+++ scripts/prerelease.js
@@ -0,0 +1,25 @@
+const path = require('node:path');
+const fs = require('node:fs');
+
+const VERSION_PATH_TS = 'src/config/constants/version.constants.ts';
+const VERSION_PATH_PACKAGE = 'package.json';
+
+const VERSION_FILE_TS = path.resolve(process.cwd(), VERSION_PATH_TS);
+const VERSION_FILE_PACKAGE = path.resolve(process.cwd(), VERSION_PATH_PACKAGE);
+const [newVersion] = process.argv.slice(2);
+
+const fileTs = fs.readFileSync(VERSION_FILE_TS, 'utf-8');
+const newVersionFileTs = fileTs.replace(/[0-9]+\.[0-9]+\.[0-9]+/, newVersion);
+
+fs.writeFileSync(VERSION_FILE_TS, newVersionFileTs);
+
+const filePackage = fs.readFileSync(VERSION_FILE_PACKAGE, 'utf-8');
+const newVersionFilePackage = filePackage.replace(
+  /\"version\": \"[0-9]+\.[0-9]+\.[0-9]+\"/,
+  `"version": "${newVersion}"`,
+);
+
+console.log(newVersionFileTs);
+console.log(newVersionFilePackage);
+
+fs.writeFileSync(VERSION_FILE_PACKAGE, newVersionFilePackage);
--- src/config/constants/version.constants.ts
+++ src/config/constants/version.constants.ts
@@ -1,1 +1,2 @@
+export const WHISPERNODE_NAME = 'whisper-tnode';
 export const WHISPERNODE_VERSION = '0.6.2';
